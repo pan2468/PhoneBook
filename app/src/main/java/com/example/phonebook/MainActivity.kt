ddfe6f4215aca6be1abceae07388fbb59a7c050b
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-03-10 21:45:06 +0900

    PhoneBook
    issue - 권한 설정
    
    complete - 권한 설정 후 체크 확인 완료

--- app/src/main/java/com/example/phonebook/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.phonebook
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		permissionCheck()
+	}
+
+	private fun permissionCheck() {
+		if(android.os.Build.VERSION.SDK_INT >= 23){
+			var permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+			if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+				ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),100)
+			}
+
+			permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+			if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+				ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 100)
+			}
+
+			permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+			if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+				ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 100)
+			}
+		}
 	}
 }
